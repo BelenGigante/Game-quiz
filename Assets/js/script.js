66fb15837d0298acd94aa9e389ed6266943c1694
BelenGigante <vinn_a@hotmail.com>
2022-12-05 19:18:04 -0500

    added js for nex and pre

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -12,6 +12,7 @@ var quiz= [
 "pregunta 4",
 ];
 
+//timer
 function timeLeft(){
     var sec= "seconds";
     if (timer === 1){
@@ -32,7 +33,21 @@ el.addEventListener("click", function setUp(){
     },1000);
 })
 //nex prev
-
+function navigate(dir){
+    index = index + dir;
+    if(index <0){
+        index = quiz.length - 1;
+    }else if (index> quiz.length -1){
+        index=0;
+    }
+    currentQuestion = quiz[index];
+    quiz.textContent = currentQuestion;
+}
+nex.addEventListener("click", function(event){
+    event.stopPropagation();
+    navigate(-1);
+});
+navigate(0);
 
 function sendMessage(){
     el.textContent="Time's Up";
