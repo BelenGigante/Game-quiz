7fd442eb9f6150cf1265b0d43221bad4faf9921c
BelenGigante <vinn_a@hotmail.com>
2022-12-05 20:15:08 -0500

    js var nex

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,8 +1,9 @@
 var el = document.querySelector(".timer");
 var timer = 60;
+var sec= "seconds";
 var question= document.querySelector(".question")
-var nex = question.querySelector(".nex");
-var pre = question.querySelector(".pre");
+var nex = document.querySelector(".nex");
+var pre = document.querySelector(".pre");
 var index = 0;
 var currentQuestion;
 var quiz= [
@@ -14,24 +15,30 @@ var quiz= [
 
 //timer
 function timeLeft(){
-    var sec= "seconds";
-    if (timer === 1){
-        sec = "second";
-    }
-    el.textContent= timer + " " + sec + " left!";
-
 }
 el.addEventListener("click", function setUp(){
     el.textContent= timer + " seconds left!";
     var inter = setInterval(function(){
         timer--;
         el.textContent = timer + " seconds left!";
+        if (timer === 1){
+            sec = "second";
+        }
+        el.textContent= timer + " " + sec + " left!";
         if (timer === 0){
             clearInterval(inter);
+            timer=60;
             sendMessage();
         }
     },1000);
 })
+function sendMessage(){
+    el.textContent="Time's Up, try again.";
+    //setUp();
+ };
+ //setUp();
+
+
 //nex prev
 function navigate(dir){
     index = index + dir;
@@ -41,16 +48,16 @@ function navigate(dir){
         index=0;
     }
     currentQuestion = quiz[index];
-    quiz.textContent = currentQuestion;
+    question.textContent = currentQuestion;
 }
+
 nex.addEventListener("click", function(event){
+    event.stopPropagation();
+    navigate(1);
+})
+pre.addEventListener("click", function(event){
     event.stopPropagation();
     navigate(-1);
 });
 navigate(0);
 
-function sendMessage(){
-    el.textContent="Time's Up";
- };
-
-setUp();
